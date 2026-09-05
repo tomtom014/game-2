6f14a2b57a2eec17b47882f7415cdcdfaa5e3d0a
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2026-09-05 15:59:06 +0400

    test island

--- scripts/Slime.gd
+++ scripts/Slime.gd
@@ -1,19 +1,32 @@
 extends CharacterBody2D
 
-var max_speed = 20
-var acceleration = 300
+@export var max_speed: float = 20.0
+@export var acceleration: float = 300.0
+@export var animated_sprite: AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	if animated_sprite == null:
+		animated_sprite = get_node_or_null("AnimatedSprite2D") as AnimatedSprite2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var direction = get_direction_to_player()
 	var target_velocity = max_speed * direction
 	velocity = velocity.move_toward(target_velocity,acceleration * delta)
 	move_and_slide()
+	update_animation()
+
+
+func update_animation():
+	if animated_sprite == null:
+		return
+	if velocity.length_squared() > 0.01:
+		animated_sprite.play("run")
+		if abs(velocity.x) > 0.01:
+			animated_sprite.flip_h = velocity.x < 0
+	else:
+		animated_sprite.play("idle")
 
 func get_direction_to_player():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
