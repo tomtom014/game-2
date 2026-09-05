714bc233b3ff11286e50142a140c103bd73e02e3
thomas <thomas.tom.seifert@gmail.com>
2026-09-05 13:31:11 +0200

    slime work

--- scripts/Slime.gd
+++ scripts/Slime.gd
@@ -1,11 +1,23 @@
-extends Node
+extends CharacterBody2D
 
+var max_speed = 20
+var acceleration = 300
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	var direction = get_direction_to_player()
+	var target_velocity = max_speed * direction
+	velocity = velocity.move_toward(target_velocity,acceleration * delta)
+	move_and_slide()
+
+func get_direction_to_player():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player != null:
+		return (player.global_position - global_position).normalized()
+	return Vector2(0,0)
+	
